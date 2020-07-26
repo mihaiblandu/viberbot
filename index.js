b2342f36679c7cf422c49dc37d1ee4ad95c7a27e
mihai blandu <mihaiblandu@icloud.com>
2020-07-26 10:56:01 +0300

    first_commit

---
+++ index.js
@@ -0,0 +1,42 @@
+const ViberBot = require('viber-bot').Bot,
+  BotEvents = require('viber-bot').Events,
+  TextMessage = require('viber-bot').Message.Text,
+  express = require('express');
+const app = express();
+require("dotenv").config();
+
+//console.log(process.env);
+
+if (!process.env.BOT_ACCOUNT_TOKEN) {
+  console.log('Could not find bot account token key.');
+  return;
+}
+if (!process.env.EXPOSE_URL) {
+  console.log('Could not find exposing url');
+  return;
+}
+
+const bot = new ViberBot({
+  authToken: process.env.BOT_ACCOUNT_TOKEN,
+  name: "Mihai Robot",
+  avatar: "https://upload.wikimedia.org/wikipedia/commons/3/3d/Katze_weiss.png"
+});
+bot.on(BotEvents.SUBSCRIBED, response => {
+  response.send(new TextMessage(`Salut ${response.userProfile.name}. Eu sunt robot ${bot.name}! Poti sa intrebi.`));
+});
+bot.on(BotEvents.MESSAGE_RECEIVED, (message, response) => {
+  response.send(new TextMessage(`Salut.`));
+});
+// A simple regular expression to answer messages in the form of 'hi' and 'hello'.
+bot.onTextMessage(/^buna| salut$/i, (message, response) =>
+    response.send(new TextMessage(`Salut ${response.userProfile.name}.`)));
+const port = process.env.PORT || 3001;
+app.use("/viber/webhook", bot.middleware());
+app.listen(port, () => {
+  console.log(`Application running on port: ${port}`);
+  bot.setWebhook(`${process.env.EXPOSE_URL}/viber/webhook`).catch(error => {
+    console.log('Can not set webhook on following server. Is it running?');
+    console.error(error);
+    process.exit(1);
+  });
+});
